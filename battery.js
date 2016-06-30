2f2f646cdad68c734aa1ef9b9555b09a01efb57b
Arjo Bruijnes <arjo@weirdwater.net>
2016-06-30 11:26:09 +0200

    Adjust charge bar based on a percentage

--- battery.js
+++ battery.js
@@ -1,8 +1,11 @@
 class Battery {
-    constructor(elementId, maxHeight = 280, minHeight = 25) {
-      this.el = document.getElementById(elementId);
-      this.maxVal = maxHeight - minHeight;
+    constructor(top, right, left, maxHeight = 170, minHeight = 0) {
+      this.top = document.getElementById(top);
+      this.right = document.getElementById(right);
+      this.left = document.getElementById(left);
+      this.maxVal = maxHeight;
       this.minVal = minHeight;
+      this.startVal = -107;
     }
 
     setPercentage(percentage) {
@@ -10,33 +13,44 @@ class Battery {
       let pixelAmount = (this.maxVal * percentage) / 100;
       pixelAmount += this.minVal;
 
-      this.updateBattery(pixelAmount, status);
+      this.updateTop(pixelAmount, status);
+      this.updateRight(pixelAmount, status);
+      this.updateLeft(pixelAmount, status);
+    }
+
+    updateTop(pixelAmount, status) {
+      this.top.setAttribute('data-status', status);
+      this.top.style.transform = 'translateZ('+ pixelAmount +'px)';
+    }
+
+    updateRight(pixelAmount, status) {
+      const translateZ = this.startVal + (pixelAmount / 2);
+      this.right.setAttribute('data-status', status);
+      this.right.style.height = pixelAmount + 'px';
+      this.right.style.transform = 'rotateX(90deg)  translateY(50%) translateZ('+ translateZ +'px)';
+    }
+
+    updateLeft(pixelAmount, status) {
+      const translateZ = this.startVal + (pixelAmount / 2);
+      this.left.setAttribute('data-status', status);
+      this.left.style.width = pixelAmount + 'px';
+      this.left.style.transform = 'rotateY(-90deg)  translateX(50%) translateZ('+ translateZ +'px)';
     }
 
     determineStatus(percentage) {
-      let status = "critical";
+      let status = 'critical';
       if (percentage > 80) {
-        status = "full";
+        status = 'full';
       }
       else if (percentage > 40) {
-        status = "normal";
+        status = 'normal';
       }
       else if (percentage > 20) {
-        status = "low";
+        status = 'low';
       }
       return status;
     }
-
-    updateBattery(pixelAmount, status) {
-      this.el.setAttribute("data-status", status);
-      this.el.setAttribute("data-height", pixelAmount);
-      // this.el.style.height = pixelAmount;
-    }
-
-    setGlow(glowOn) {
-      this.el.setAttribute("data-glow", glowOn);
-    }
 }
 
-const batt = new Battery("battery");
+const batt = new Battery('isoCubeTop', 'isoCubeRight', 'isoCubeLeft');
 batt.setPercentage(30);
